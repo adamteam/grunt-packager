c51e77759101449f11f86a998bde85aeb81277ed
Alex Tseng <exqy.com@gmail.com>
2014-08-25 20:49:02 +0800

    Created gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+/*
+ * grunt-packager
+ * https://github.com/adamteam/grunt-packager
+ *
+ * Copyright (c) 2014 Alex Tseng
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadTasks('tasks');
+
+};
